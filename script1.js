199f9785d6e8b46db7e44b3951b208dd07c5fb64
Beant-Kaur <beant8410@gmail.com>
2026-04-06 05:07:53 +0530

    fix dashboard layout, improve cards responsiveness, and resolve JS bugs

--- script1.js
+++ script1.js
@@ -1,17 +1,22 @@
 function loadData() {
+  const container = document.getElementById("data");
+  container.innerText = "Loading...";
   fetch('https://jsonplaceholder.typicode.com/users')
     .then(function(res) {
       return res.json();
     })
     .then(function(data) {
-      for (var i = 0; i < data.length; i++) {
+       container.innerHTML = "";
+      for (let i = 0; i < data.length; i++) {
         setTimeout(function() {
-          document.getElementById("data").innerHTML += "<p>" + data[i].name + "</p>";
-        }, 1000);
-        
+          const container = document.getElementById("data") 
+          const p = document.createElement("p");
+          p.textContent = data[i].name;
+          container.appendChild(p);
+        }, i* 1000);
       }
     })
     .catch(function(err) {
-      console.log(err);
+      console.innerText = "Failed to load data";
     });
 }
